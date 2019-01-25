c903ebb65c64383b1694ed79ebf9767be3350b58
Luke Howard <15623020@students.lincoln.ac.uk>
2019-01-25 23:52:28 +0000

    Room1 Scene
    basic room with crate, large crate, table, lamp, laser trap, monitor, desk, and player prefabs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(MoveHorizontal*speed, MoveVertical*speed,0);
+
+
+    }
+}
