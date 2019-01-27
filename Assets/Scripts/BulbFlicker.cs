7b6df7f140caba9cb5118e2389cf0f64e6757f93
Luke Howard <15623020@students.lincoln.ac.uk>
2019-01-27 15:25:00 +0000

    Added Main Menu with sound and added sound to level
    as above

---
+++ Assets/Scripts/BulbFlicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbFlicker : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool flickering = false;
+    private IEnumerator FlickerCoroutine;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!flickering)
+        {
+            float ontime = Random.Range(0.2f, 4.0f);
+            float offtime = Random.Range(0.01f, 0.1f);
+            FlickerCoroutine = Flicker(ontime, offtime);
+            StartCoroutine(FlickerCoroutine);
+        }
+    }
+
+    private IEnumerator Flicker(float ondelay, float offdelay)
+    {
+        flickering = true;
+        yield return new WaitForSeconds(ondelay);
+        rb.transform.Find("Light Bulb").gameObject.SetActive(false);
+        yield return new WaitForSeconds(offdelay);
+        rb.transform.Find("Light Bulb").gameObject.SetActive(true);
+        flickering = false;
+    }
+
+}
+
